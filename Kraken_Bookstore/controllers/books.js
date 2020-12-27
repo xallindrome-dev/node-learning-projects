0a004345970f1f1426886cb2ade6a108c776382c
Andrew <dna.dub.subscriptions@outlook.com>
2020-12-27 09:56:45 -0800

    Added Kraken bookstore with Node

---
+++ Kraken_Bookstore/controllers/books.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var Book = require("../models/bookModel");
+var category = require("../models/categoryModel");
+
+module.exports = function(router) {
+    router.get("/", function(req, res) {// /books
+        res.render("index");
+    }); 
+
+    // /books/details/id
+    router.get("/details/:id", function(req, res) {
+        Book.findOne({ _id: req.params.id }, function(err, book) {
+            if(err) {
+                console.log(err);
+            }
+
+            var model = {
+                book: book
+            };
+
+            res.render("books/details", model);
+        });
+    }); 
+}
